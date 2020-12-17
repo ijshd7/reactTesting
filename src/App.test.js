dbb9d84bc072f1380d600d79879c2aab4669ed91
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-12-17 01:43:07 -0600

    starting to add testing code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Header', () => {
+  test('"How it works" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const link = screen.getByRole('link', { name: /how it works/i });
+    screen.debug(link);
+  });
 });
