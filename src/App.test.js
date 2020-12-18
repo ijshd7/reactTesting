8e0de7a0f86cec73bc1b841522fdc73d617e0fa7
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-12-18 01:02:18 -0600

    first test suite finished

--- src/App.test.js
+++ src/App.test.js
@@ -20,4 +20,32 @@ describe('Header', () => {
       screen.getByRole('heading', { name: /how it works/i })
     ).toBeInTheDocument();
   });
+
+  test('"About" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const link = screen.getByRole('link', { name: /about/i });
+    userEvent.click(link);
+    
+    expect(
+      screen.getByRole('heading', { name: /about/i })
+    ).toBeInTheDocument();
+  });
+
+  test('"Logo" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const link = screen.getByRole('link', { name: /logo/i });
+    userEvent.click(link);
+    
+    expect(
+      screen.getByRole('heading', { name: /find the top posts on reddit/i })
+    ).toBeInTheDocument();
+  });
 });
