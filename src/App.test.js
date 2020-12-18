75ffdddfaae3492fc97e87fd5bd5f7ece6488714
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-12-18 00:17:53 -0600

    first test set up fully

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from "react-router-dom";
+import userEvent from '@testing-library/user-event';
 import App from './App';
+
+// describe method not necessary, but good for grouping together suite of tests
 
 describe('Header', () => {
   test('"How it works" link points to the correct page', () => {
@@ -11,6 +14,10 @@ describe('Header', () => {
       </MemoryRouter>
     );
     const link = screen.getByRole('link', { name: /how it works/i });
-    screen.debug(link);
+    userEvent.click(link);
+    
+    expect(
+      screen.getByRole('heading', { name: /how it works/i })
+    ).toBeInTheDocument();
   });
 });
